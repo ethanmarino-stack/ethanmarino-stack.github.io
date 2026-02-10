a89dffc25f05f5ff8ddc1a73642976fa9a735743
ethanmarino-stack <ethansmarino@gmail.com>
2026-02-10 13:17:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ document.addEventListener('keydown', e => {
   }
 });
 
+// Mobile nav toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
